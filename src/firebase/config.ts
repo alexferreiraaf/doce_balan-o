a7f1261ae83b673a314d5655cca0e3287eab3070
Alex Ferreira <alexferreira.AF411@gmail.com>
2025-10-21 16:21:13 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3851635010-feffc",
-  "appId": "1:577267387381:web:03909d4e5f03230acf2ebc",
-  "apiKey": "AIzaSyCRnL6BL05yha_EU2gn3vfopYg-Qbn48j0",
-  "authDomain": "studio-3851635010-feffc.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "577267387381"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
