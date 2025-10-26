0684e24cd0d13cb7e3b29957abdff444e5fb8df8
Alex Ferreira <alexferreira.AF411@gmail.com>
2025-10-26 15:06:43 +0000

    Eu quero substituir por esse ícone

--- src/components/icons/whisk-icon.tsx
+++ src/components/icons/whisk-icon.tsx
@@ -13,11 +13,10 @@ export const WhiskIcon = (props: SVGProps<SVGSVGElement>) => (
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <path d="M8 18a2 2 0 0 0-2 2h12a2 2 0 0 0-2-2Z" />
-    <path d="M8 18V7.5c0-2 2-3.5 4-3.5s4 1.5 4 3.5V18" />
-    <path d="M12 4v2" />
-    <path d="M12 11h.01" />
-    <path d="M15 7.5c0 3.11-2.5 5.5-2.5 5.5" />
-    <path d="M9 7.5c0 3.11 2.5 5.5 2.5 5.5" />
+    <path d="M12 17.5a2.5 2.5 0 0 1-2.5-2.5V12h5v3a2.5 2.5 0 0 1-2.5 2.5Z" />
+    <path d="M12 17.5v2.5" />
+    <path d="M12 12a5 5 0 0 1 5-5" />
+    <path d="M7 7a5 5 0 0 1 5-5" />
+    <path d="M12 12H7a5 5 0 0 0-5 5v0a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v0a5 5 0 0 0-5-5h-5Z" />
   </svg>
 );
