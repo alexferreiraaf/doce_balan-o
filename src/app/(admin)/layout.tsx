9ddebb9e800090e23f43e0517a881d11d000dd02
Alex Ferreira <alexferreira.AF411@gmail.com>
2026-01-07 10:54:39 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import { useUser } from '@/firebase';
-import Loading from './loading';
+import Loading from '@/app/(admin)/loading';
 import { Navbar } from '@/components/layout/navbar';
 import { cn } from '@/lib/utils';
 
