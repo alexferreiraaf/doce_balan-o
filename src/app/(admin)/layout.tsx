011e96de119b366189bbd614f0d1bccf9bc1f281
alexferreiraaf <alexferreira.af411@gmail.com>
2026-05-24 09:43:01 -0300

    fix: update Super Admin validation to include primary email

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -22,7 +22,7 @@ export default function AdminLayout({
     if (isUserLoading) {
       return; 
     }
-    const isSuperAdmin = user?.uid === 'I7HKJVmF4uRDL75dQShPmHVi4333';
+    const isSuperAdmin = user?.uid === 'I7HKJVmF4uRDL75dQShPmHVi4333' || user?.email === 'alexferreira.af411@gmail.com';
     const isEmployee = userRole === 'admin' || userRole === 'user';
 
     // Se não estiver carregando e o usuário não existir, for anônimo, ou não tiver permissão, redireciona para o login.
@@ -31,7 +31,7 @@ export default function AdminLayout({
     }
   }, [user, userRole, isUserLoading, router]);
 
-  const isSuperAdmin = user?.uid === 'I7HKJVmF4uRDL75dQShPmHVi4333';
+  const isSuperAdmin = user?.uid === 'I7HKJVmF4uRDL75dQShPmHVi4333' || user?.email === 'alexferreira.af411@gmail.com';
   const isEmployee = userRole === 'admin' || userRole === 'user';
 
   // Mostra a tela de carregamento enquanto verifica o usuário ou se o usuário não for válido.
